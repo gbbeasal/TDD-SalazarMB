d87b5d20b04074c683fa4daddc10eec27049d994
gbbeasal <ma.salazar@globe.com.ph>
2022-07-21 06:16:26 +0000

    Added new TDD Labs folder

---
+++ TDD_Labs/test_lab1.py
@@ -0,0 +1,15 @@
+
+def increment_by_one(i):
+    return i+1
+    
+
+def test_answer_inc():
+    assert increment_by_one(1) == 2
+    
+
+def decrement_by_one(i):
+    return i-1
+    
+    
+def test_answer_dec():
+    assert decrement_by_one(1) == 0
---
+++ TDD_Labs/test_lab5.py
@@ -0,0 +1,13 @@
+import pytest
+
+def divide(a, b):
+    return a / b
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        print("Trying to do division by 0", divide(5,0))
+
+
+def test_division():
+    print("Non-zero division", divide(10,2))
